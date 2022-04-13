8b7a4403638dd130ee0c60338e9ba7e2f43b1232
Simon Henrique <mind.traveler@hotmail.com>
2022-04-13 08:58:47 -0300

    first commit

---
+++ todo-app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AngularMaterialModule} from './angular-material.module';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import { TaskListPageComponent } from './tasks/pages/task-list-page/task-list-page.component';
+import { TaskFormPageComponent } from './tasks/pages/task-form-page/task-form-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskListPageComponent,
+    TaskFormPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ todo-app/src/app/tasks/models/itask.ts
@@ -0,0 +1,6 @@
+export interface ITask {
+  id?: string; //não é obrigatorio
+  title: string;
+  description: string;
+  done: boolean;
+}
